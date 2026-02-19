74b1ef28398adaaa25f73fffef9d7cc8a22a8aa0
Narendran-arch <kumarnarendran.sk@gmail.com>
2026-02-19 08:07:12 +0530

    build fix adding suspense

--- src/app/ourservice/ourtherapies/page.js
+++ src/app/ourservice/ourtherapies/page.js
@@ -1,9 +1,15 @@
+"use client";
+
+import { Suspense } from "react";
 import Ourtherapies from '@/components/layout/Ourtherapiescomp.js'
 import React from 'react'
 
 const page = () => {
   return (
+     <Suspense fallback={null}>
+      <OurTherapiesInner />
     <div><Ourtherapies/></div>
+    </Suspense>
   )
 }
 
